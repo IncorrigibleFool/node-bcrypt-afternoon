150fdab1dd58a010270ba0b6f982aabeaba10754
Brian Gardiner <bgardiner9001@gmail.com>
2019-04-25 16:20:11 -0600

    on step 6

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./controllers/authCtrl')
+const treasureCtrl = require('./controllers/treasureCtrl')
+
+const PORT = 4000
+const{CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false
+}))
+
+massive(CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log('Database connected.')
+})
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.delete('/auth/logout', authCtrl.logout)
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+
+//listener
+app.listen(PORT, ()=> {
+    console.log(`Listening on port: ${PORT}`)
+})
